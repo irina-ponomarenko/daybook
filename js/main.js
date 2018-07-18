a69037d5e92ff9ff7d88558815c2b1bef7d7cd1b
Admin <ponomarenko02041993@gmail.com>
2018-07-18 23:15:38 +0300

    new

--- js/main.js
+++ js/main.js
@@ -7,4 +7,25 @@ $(document).ready(function () {
             }
         });
     });
+    $('.parents_open').on('click', function() {
+        $('#parents').popup({
+            transition: 'all 0.3s',
+            scrolllock: true,
+            onclose: function () {
+            }
+        });
+    });
+
+    $(function(){
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-36251023-1']);
+        _gaq.push(['_setDomainName', 'jqueryscript.net']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    })
 });
