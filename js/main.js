20db0cb1fe0f14f1b1a4ad58d5b080e86c324a44
Admin <ponomarenko02041993@gmail.com>
2018-07-19 23:42:00 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
-    $('.fade_open').on('click', function() {
-        $('#fade').popup({
+
+    //------------------------модальні вікна------------------//
+
+
+    $('.teacher_open').on('click', function() {
+        $('#teacher').popup({
             transition: 'all 0.3s',
             scrolllock: true,
             onclose: function () {
@@ -9,6 +13,33 @@ $(document).ready(function () {
     });
     $('.parents_open').on('click', function() {
         $('#parents').popup({
+            transition: 'all 0.3s',
+            scrolllock: true,
+            onclose: function () {
+            }
+        });
+    });
+
+    $('.student_open').on('click', function() {
+        $('#student').popup({
+            transition: 'all 0.3s',
+            scrolllock: true,
+            onclose: function () {
+            }
+        });
+    });
+
+    $('.user_open').on('click', function() {
+        $('#user').popup({
+            transition: 'all 0.3s',
+            scrolllock: true,
+            onclose: function () {
+            }
+        });
+    });
+
+    $('.login_open').on('click', function() {
+        $('#login').popup({
             transition: 'all 0.3s',
             scrolllock: true,
             onclose: function () {
@@ -27,5 +58,22 @@ $(document).ready(function () {
             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
         })();
-    })
+    });
+
+
+    $('.hint-js').focus(function() {
+        $(this)
+            .parent()
+            .find('.hint')
+            .addClass('hint-active');
+    });
+
+    $('.hint-js').on('blur', function() {
+        setTimeout(() => {
+            $(this)
+                .parent()
+                .find('.hint')
+                .removeClass('hint-active');
+        }, 100);
+    });
 });
